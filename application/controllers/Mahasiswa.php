3f78794beced2be98b64e5752da93b6c4c0e0c1b
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-10-23 20:31:25 +0700

    Membuat aplikasi menggunakan CI dan bootstrap

---
+++ application/controllers/Mahasiswa.php
@@ -0,0 +1,24 @@
+<?php
+class Mahasiswa extends CI_Controller{
+
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('Mahasiswa_model');
+    //     $this->load->library('form_validation');
+        
+        
+    }
+
+    public function index(){
+        // 
+        $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
+        // if ($this->input->post('keyword')) {
+        //     $data['mahasiswa'] = $this->Mahasiswa_model->cariDataMahasiswa();
+        // }
+        $data['judul'] = 'Daftar Mahasiswa';
+        $this->load->view('templates/header', $data);
+        $this->load->view('mahasiswa/index', $data) ;
+        $this->load->view('templates/footer');
+    }
+}
