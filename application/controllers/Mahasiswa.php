bd629ae7b44ab6b2f4f5f10a6b4f10938eb95e07
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-10-23 20:42:34 +0700

    membuat fungsi index pada kelas Mahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -11,12 +11,13 @@ class Mahasiswa extends CI_Controller{
     }
 
     public function index(){
-        // 
-        $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
-        // if ($this->input->post('keyword')) {
-        //     $data['mahasiswa'] = $this->Mahasiswa_model->cariDataMahasiswa();
-        // }
+
         $data['judul'] = 'Daftar Mahasiswa';
+        $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
+        if ($this->input->post('keyword')) {
+            $data['mahasiswa'] = $this->Mahasiswa_model->cariDataMahasiswa();
+        }
+        
         $this->load->view('templates/header', $data);
         $this->load->view('mahasiswa/index', $data) ;
         $this->load->view('templates/footer');
