851bb2fc6f4027dad89f0b900eb0ba77e57bdf35
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-10-23 20:40:46 +0700

    Membuat fungsi constructor pada kelas Mahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -5,7 +5,7 @@ class Mahasiswa extends CI_Controller{
     public function __construct(){
         parent::__construct();
         $this->load->model('Mahasiswa_model');
-    //     $this->load->library('form_validation');
+        $this->load->library('form_validation');
         
         
     }
