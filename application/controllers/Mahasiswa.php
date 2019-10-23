1c7151d311eb1f5e32ff48e601f52c36710e075e
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-10-23 20:45:30 +0700

    menambahkan fungsi hapus pada kelas Mahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -39,4 +39,10 @@ class Mahasiswa extends CI_Controller{
             redirect('mahasiswa');
         }   
     }
+
+    public function hapus($id){
+        $this->Mahasiswa_model->hapusDataMahasiswa($id);
+        $this->session->set_flashdata('flash', 'Dihapus');
+        redirect('mahasiswa');
+    }
 }
